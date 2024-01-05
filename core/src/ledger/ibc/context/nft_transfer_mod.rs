4c0fb15461f6311b10697850ca8e3d3729c849a1
yito88 <yuji@phact-columba.com>
2024-01-06 00:05:48 +0100

    add nft send test

---
+++ core/src/ledger/ibc/context/nft_transfer_mod.rs
@@ -0,0 +1,522 @@
+//! IBC module for token transfer
+
+use std::cell::RefCell;
+use std::fmt::Debug;
+use std::rc::Rc;
+
+use super::common::IbcCommonContext;
+use super::nft_transfer::NftTransferContext;
+use super::transfer_mod::ModuleWrapper;
+use crate::ibc::apps::nft_transfer::context::NftTransferValidationContext;
+use crate::ibc::apps::nft_transfer::module::{
+    on_acknowledgement_packet_execute, on_acknowledgement_packet_validate,
+    on_chan_close_confirm_execute, on_chan_close_confirm_validate,
+    on_chan_close_init_execute, on_chan_close_init_validate,
+    on_chan_open_ack_execute, on_chan_open_ack_validate,
+    on_chan_open_confirm_execute, on_chan_open_confirm_validate,
+    on_chan_open_init_execute, on_chan_open_init_validate,
+    on_chan_open_try_execute, on_chan_open_try_validate,
+    on_recv_packet_execute, on_timeout_packet_execute,
+    on_timeout_packet_validate,
+};
+use crate::ibc::apps::nft_transfer::types::error::NftTransferError;
+use crate::ibc::apps::nft_transfer::types::MODULE_ID_STR;
+use crate::ibc::core::channel::types::acknowledgement::Acknowledgement;
+use crate::ibc::core::channel::types::channel::{Counterparty, Order};
+use crate::ibc::core::channel::types::error::{ChannelError, PacketError};
+use crate::ibc::core::channel::types::packet::Packet;
+use crate::ibc::core::channel::types::Version;
+use crate::ibc::core::host::types::identifiers::{
+    ChannelId, ConnectionId, PortId,
+};
+use crate::ibc::core::router::module::Module;
+use crate::ibc::core::router::types::module::{ModuleExtras, ModuleId};
+use crate::ibc::primitives::Signer;
+
+/// IBC module for NFT transfer
+#[derive(Debug)]
+pub struct NftTransferModule<C>
+where
+    C: IbcCommonContext,
+{
+    /// IBC actions
+    pub ctx: NftTransferContext<C>,
+}
+
+impl<C> NftTransferModule<C>
+where
+    C: IbcCommonContext,
+{
+    /// Make a new module
+    pub fn new(ctx: Rc<RefCell<C>>) -> Self {
+        Self {
+            ctx: NftTransferContext::new(ctx),
+        }
+    }
+}
+
+impl<C> ModuleWrapper for NftTransferModule<C>
+where
+    C: IbcCommonContext + Debug,
+{
+    fn as_module(&self) -> &dyn Module {
+        self
+    }
+
+    fn as_module_mut(&mut self) -> &mut dyn Module {
+        self
+    }
+
+    fn module_id(&self) -> ModuleId {
+        ModuleId::new(MODULE_ID_STR.to_string())
+    }
+
+    fn port_id(&self) -> PortId {
+        self.ctx.get_port().expect("The port ID should be set")
+    }
+}
+
+impl<C> Module for NftTransferModule<C>
+where
+    C: IbcCommonContext + Debug,
+{
+    #[allow(clippy::too_many_arguments)]
+    fn on_chan_open_init_validate(
+        &self,
+        order: Order,
+        connection_hops: &[ConnectionId],
+        port_id: &PortId,
+        channel_id: &ChannelId,
+        counterparty: &Counterparty,
+        version: &Version,
+    ) -> Result<Version, ChannelError> {
+        on_chan_open_init_validate(
+            &self.ctx,
+            order,
+            connection_hops,
+            port_id,
+            channel_id,
+            counterparty,
+            version,
+        )
+        .map_err(into_channel_error)?;
+        Ok(version.clone())
+    }
+
+    #[allow(clippy::too_many_arguments)]
+    fn on_chan_open_init_execute(
+        &mut self,
+        order: Order,
+        connection_hops: &[ConnectionId],
+        port_id: &PortId,
+        channel_id: &ChannelId,
+        counterparty: &Counterparty,
+        version: &Version,
+    ) -> Result<(ModuleExtras, Version), ChannelError> {
+        on_chan_open_init_execute(
+            &mut self.ctx,
+            order,
+            connection_hops,
+            port_id,
+            channel_id,
+            counterparty,
+            version,
+        )
+        .map_err(into_channel_error)
+    }
+
+    #[allow(clippy::too_many_arguments)]
+    fn on_chan_open_try_validate(
+        &self,
+        order: Order,
+        connection_hops: &[ConnectionId],
+        port_id: &PortId,
+        channel_id: &ChannelId,
+        counterparty: &Counterparty,
+        counterparty_version: &Version,
+    ) -> Result<Version, ChannelError> {
+        on_chan_open_try_validate(
+            &self.ctx,
+            order,
+            connection_hops,
+            port_id,
+            channel_id,
+            counterparty,
+            counterparty_version,
+        )
+        .map_err(into_channel_error)?;
+        Ok(counterparty_version.clone())
+    }
+
+    #[allow(clippy::too_many_arguments)]
+    fn on_chan_open_try_execute(
+        &mut self,
+        order: Order,
+        connection_hops: &[ConnectionId],
+        port_id: &PortId,
+        channel_id: &ChannelId,
+        counterparty: &Counterparty,
+        counterparty_version: &Version,
+    ) -> Result<(ModuleExtras, Version), ChannelError> {
+        on_chan_open_try_execute(
+            &mut self.ctx,
+            order,
+            connection_hops,
+            port_id,
+            channel_id,
+            counterparty,
+            counterparty_version,
+        )
+        .map_err(into_channel_error)
+    }
+
+    fn on_chan_open_ack_validate(
+        &self,
+        port_id: &PortId,
+        channel_id: &ChannelId,
+        counterparty_version: &Version,
+    ) -> Result<(), ChannelError> {
+        on_chan_open_ack_validate(
+            &self.ctx,
+            port_id,
+            channel_id,
+            counterparty_version,
+        )
+        .map_err(into_channel_error)
+    }
+
+    fn on_chan_open_ack_execute(
+        &mut self,
+        port_id: &PortId,
+        channel_id: &ChannelId,
+        counterparty_version: &Version,
+    ) -> Result<ModuleExtras, ChannelError> {
+        on_chan_open_ack_execute(
+            &mut self.ctx,
+            port_id,
+            channel_id,
+            counterparty_version,
+        )
+        .map_err(into_channel_error)
+    }
+
+    fn on_chan_open_confirm_validate(
+        &self,
+        port_id: &PortId,
+        channel_id: &ChannelId,
+    ) -> Result<(), ChannelError> {
+        on_chan_open_confirm_validate(&self.ctx, port_id, channel_id)
+            .map_err(into_channel_error)
+    }
+
+    fn on_chan_open_confirm_execute(
+        &mut self,
+        port_id: &PortId,
+        channel_id: &ChannelId,
+    ) -> Result<ModuleExtras, ChannelError> {
+        on_chan_open_confirm_execute(&mut self.ctx, port_id, channel_id)
+            .map_err(into_channel_error)
+    }
+
+    fn on_chan_close_init_validate(
+        &self,
+        port_id: &PortId,
+        channel_id: &ChannelId,
+    ) -> Result<(), ChannelError> {
+        on_chan_close_init_validate(&self.ctx, port_id, channel_id)
+            .map_err(into_channel_error)
+    }
+
+    fn on_chan_close_init_execute(
+        &mut self,
+        port_id: &PortId,
+        channel_id: &ChannelId,
+    ) -> Result<ModuleExtras, ChannelError> {
+        on_chan_close_init_execute(&mut self.ctx, port_id, channel_id)
+            .map_err(into_channel_error)
+    }
+
+    fn on_chan_close_confirm_validate(
+        &self,
+        port_id: &PortId,
+        channel_id: &ChannelId,
+    ) -> Result<(), ChannelError> {
+        on_chan_close_confirm_validate(&self.ctx, port_id, channel_id)
+            .map_err(into_channel_error)
+    }
+
+    fn on_chan_close_confirm_execute(
+        &mut self,
+        port_id: &PortId,
+        channel_id: &ChannelId,
+    ) -> Result<ModuleExtras, ChannelError> {
+        on_chan_close_confirm_execute(&mut self.ctx, port_id, channel_id)
+            .map_err(into_channel_error)
+    }
+
+    fn on_recv_packet_execute(
+        &mut self,
+        packet: &Packet,
+        _relayer: &Signer,
+    ) -> (ModuleExtras, Acknowledgement) {
+        on_recv_packet_execute(&mut self.ctx, packet)
+    }
+
+    fn on_acknowledgement_packet_validate(
+        &self,
+        packet: &Packet,
+        acknowledgement: &Acknowledgement,
+        relayer: &Signer,
+    ) -> Result<(), PacketError> {
+        on_acknowledgement_packet_validate(
+            &self.ctx,
+            packet,
+            acknowledgement,
+            relayer,
+        )
+        .map_err(into_packet_error)
+    }
+
+    fn on_acknowledgement_packet_execute(
+        &mut self,
+        packet: &Packet,
+        acknowledgement: &Acknowledgement,
+        relayer: &Signer,
+    ) -> (ModuleExtras, Result<(), PacketError>) {
+        let (extras, result) = on_acknowledgement_packet_execute(
+            &mut self.ctx,
+            packet,
+            acknowledgement,
+            relayer,
+        );
+        (extras, result.map_err(into_packet_error))
+    }
+
+    fn on_timeout_packet_validate(
+        &self,
+        packet: &Packet,
+        relayer: &Signer,
+    ) -> Result<(), PacketError> {
+        on_timeout_packet_validate(&self.ctx, packet, relayer)
+            .map_err(into_packet_error)
+    }
+
+    fn on_timeout_packet_execute(
+        &mut self,
+        packet: &Packet,
+        relayer: &Signer,
+    ) -> (ModuleExtras, Result<(), PacketError>) {
+        let (extras, result) =
+            on_timeout_packet_execute(&mut self.ctx, packet, relayer);
+        (extras, result.map_err(into_packet_error))
+    }
+}
+
+fn into_channel_error(error: NftTransferError) -> ChannelError {
+    ChannelError::AppModule {
+        description: error.to_string(),
+    }
+}
+
+fn into_packet_error(error: NftTransferError) -> PacketError {
+    PacketError::AppModule {
+        description: error.to_string(),
+    }
+}
+
+/// Helpers for testing
+#[cfg(any(test, feature = "testing"))]
+pub mod testing {
+    use std::str::FromStr;
+
+    use super::*;
+    use crate::ibc::apps::nft_transfer::types::{ack_success_b64, PORT_ID_STR};
+    use crate::ibc::core::channel::types::acknowledgement::AcknowledgementStatus;
+
+    /// Dummy IBC module for token transfer
+    #[derive(Debug)]
+    pub struct DummyNftTransferModule {}
+
+    impl ModuleWrapper for DummyNftTransferModule {
+        fn as_module(&self) -> &dyn Module {
+            self
+        }
+
+        fn as_module_mut(&mut self) -> &mut dyn Module {
+            self
+        }
+
+        fn module_id(&self) -> ModuleId {
+            ModuleId::new(MODULE_ID_STR.to_string())
+        }
+
+        fn port_id(&self) -> PortId {
+            PortId::from_str(PORT_ID_STR).unwrap()
+        }
+    }
+
+    impl Module for DummyNftTransferModule {
+        #[allow(clippy::too_many_arguments)]
+        fn on_chan_open_init_validate(
+            &self,
+            _order: Order,
+            _connection_hops: &[ConnectionId],
+            _port_id: &PortId,
+            _channel_id: &ChannelId,
+            _counterparty: &Counterparty,
+            version: &Version,
+        ) -> Result<Version, ChannelError> {
+            Ok(version.clone())
+        }
+
+        #[allow(clippy::too_many_arguments)]
+        fn on_chan_open_init_execute(
+            &mut self,
+            _order: Order,
+            _connection_hops: &[ConnectionId],
+            _port_id: &PortId,
+            _channel_id: &ChannelId,
+            _counterparty: &Counterparty,
+            version: &Version,
+        ) -> Result<(ModuleExtras, Version), ChannelError> {
+            Ok((ModuleExtras::empty(), version.clone()))
+        }
+
+        #[allow(clippy::too_many_arguments)]
+        fn on_chan_open_try_validate(
+            &self,
+            _order: Order,
+            _connection_hops: &[ConnectionId],
+            _port_id: &PortId,
+            _channel_id: &ChannelId,
+            _counterparty: &Counterparty,
+            counterparty_version: &Version,
+        ) -> Result<Version, ChannelError> {
+            Ok(counterparty_version.clone())
+        }
+
+        #[allow(clippy::too_many_arguments)]
+        fn on_chan_open_try_execute(
+            &mut self,
+            _order: Order,
+            _connection_hops: &[ConnectionId],
+            _port_id: &PortId,
+            _channel_id: &ChannelId,
+            _counterparty: &Counterparty,
+            counterparty_version: &Version,
+        ) -> Result<(ModuleExtras, Version), ChannelError> {
+            Ok((ModuleExtras::empty(), counterparty_version.clone()))
+        }
+
+        fn on_chan_open_ack_validate(
+            &self,
+            _port_id: &PortId,
+            _channel_id: &ChannelId,
+            _counterparty_version: &Version,
+        ) -> Result<(), ChannelError> {
+            Ok(())
+        }
+
+        fn on_chan_open_ack_execute(
+            &mut self,
+            _port_id: &PortId,
+            _channel_id: &ChannelId,
+            _counterparty_version: &Version,
+        ) -> Result<ModuleExtras, ChannelError> {
+            Ok(ModuleExtras::empty())
+        }
+
+        fn on_chan_open_confirm_validate(
+            &self,
+            _port_id: &PortId,
+            _channel_id: &ChannelId,
+        ) -> Result<(), ChannelError> {
+            Ok(())
+        }
+
+        fn on_chan_open_confirm_execute(
+            &mut self,
+            _port_id: &PortId,
+            _channel_id: &ChannelId,
+        ) -> Result<ModuleExtras, ChannelError> {
+            Ok(ModuleExtras::empty())
+        }
+
+        fn on_chan_close_init_validate(
+            &self,
+            _port_id: &PortId,
+            _channel_id: &ChannelId,
+        ) -> Result<(), ChannelError> {
+            Ok(())
+        }
+
+        fn on_chan_close_init_execute(
+            &mut self,
+            _port_id: &PortId,
+            _channel_id: &ChannelId,
+        ) -> Result<ModuleExtras, ChannelError> {
+            Ok(ModuleExtras::empty())
+        }
+
+        fn on_chan_close_confirm_validate(
+            &self,
+            _port_id: &PortId,
+            _channel_id: &ChannelId,
+        ) -> Result<(), ChannelError> {
+            Ok(())
+        }
+
+        fn on_chan_close_confirm_execute(
+            &mut self,
+            _port_id: &PortId,
+            _channel_id: &ChannelId,
+        ) -> Result<ModuleExtras, ChannelError> {
+            Ok(ModuleExtras::empty())
+        }
+
+        fn on_recv_packet_execute(
+            &mut self,
+            _packet: &Packet,
+            _relayer: &Signer,
+        ) -> (ModuleExtras, Acknowledgement) {
+            (
+                ModuleExtras::empty(),
+                AcknowledgementStatus::success(ack_success_b64()).into(),
+            )
+        }
+
+        fn on_acknowledgement_packet_validate(
+            &self,
+            _packet: &Packet,
+            _acknowledgement: &Acknowledgement,
+            _relayer: &Signer,
+        ) -> Result<(), PacketError> {
+            Ok(())
+        }
+
+        fn on_acknowledgement_packet_execute(
+            &mut self,
+            _packet: &Packet,
+            _acknowledgement: &Acknowledgement,
+            _relayer: &Signer,
+        ) -> (ModuleExtras, Result<(), PacketError>) {
+            (ModuleExtras::empty(), Ok(()))
+        }
+
+        fn on_timeout_packet_validate(
+            &self,
+            _packet: &Packet,
+            _relayer: &Signer,
+        ) -> Result<(), PacketError> {
+            Ok(())
+        }
+
+        fn on_timeout_packet_execute(
+            &mut self,
+            _packet: &Packet,
+            _relayer: &Signer,
+        ) -> (ModuleExtras, Result<(), PacketError>) {
+            (ModuleExtras::empty(), Ok(()))
+        }
+    }
+}
