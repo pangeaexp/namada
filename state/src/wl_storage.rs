676d215eb150c09f62cfaa1aa043486c78517a4e
Tomáš Zemanovič <tomas@heliax.dev>
2024-01-10 13:54:39 +0000

    fix `get_current_decision_height` by moving it to WlStorage

--- state/src/wl_storage.rs
+++ state/src/wl_storage.rs
@@ -259,6 +259,28 @@ where
     pub fn delete_tx_hash(&mut self, hash: Hash) -> write_log::Result<()> {
         self.write_log.delete_tx_hash(hash)
     }
+
+    #[inline]
+    pub fn get_current_decision_height(&self) -> BlockHeight {
+        self.storage.get_last_block_height() + 1
+    }
+
+    /// Check if we are at a given [`BlockHeight`] offset, `height_offset`,
+    /// within the current epoch.
+    pub fn is_deciding_offset_within_epoch(&self, height_offset: u64) -> bool {
+        let current_decision_height = self.get_current_decision_height();
+
+        let pred_epochs = &self.storage.block.pred_epochs;
+        let fst_heights_of_each_epoch = pred_epochs.first_block_heights();
+
+        fst_heights_of_each_epoch
+            .last()
+            .map(|&h| {
+                let height_offset_within_epoch = h + height_offset;
+                current_decision_height == height_offset_within_epoch
+            })
+            .unwrap_or(false)
+    }
 }
 
 /// Prefix iterator for [`WlStorage`].
